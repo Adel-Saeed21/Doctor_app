681c03a3e849d84cdd6583b256da89811f522aa4
Adool <adelsaid284@gmail.com>
2025-06-09 19:00:37 +0300

    on boarding screen

--- lib/features/on_boarding/onboardingScreen.dart
+++ lib/features/on_boarding/onboardingScreen.dart
@@ -1,4 +1,11 @@
+// ignore_for_file: file_names
+
+import 'package:advanced/core/theming/style.dart';
+import 'package:advanced/features/on_boarding/widgets/doc_img_and_text.dart';
+import 'package:advanced/features/on_boarding/widgets/doc_logo_and_name.dart';
+import 'package:advanced/features/on_boarding/widgets/get_started_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Onboardingscreen extends StatelessWidget {
   const Onboardingscreen({super.key});
@@ -6,8 +13,35 @@ class Onboardingscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: const Text("On boarding screen"),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(top: 30.h, bottom: 30.h),
+            child: Column(
+              children: [
+                DocLogoAndName(),
+                SizedBox(height: 30.h),
+                const DocImgAndText(),
+                Padding(
+                  padding: EdgeInsets.only(left: 20.w, right: 20.w),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        style: TextStyles.font10Gery,
+                        textAlign: TextAlign.center,
+                        "Manage and schedule all of your medical appointments easily with Docdoc to get a new experience",
+                      ),
+
+                      SizedBox(height: 30.h),
+                      const GetStartedButton(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
